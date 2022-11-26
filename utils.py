0f3a9a4f16c33e1e3a36d5e6f4fb82218caefc8a
technicalaks <105633032+technicalaks@users.noreply.github.com>
2022-11-26 12:55:35 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from info import *
